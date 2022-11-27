a9a3a5edde0d558ef1f7caf8ae3a320427107db1
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2022-11-27 22:47:36 +0600

    Added first posts page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Local imports.
+import HeadMeta from '../components/head-meta';
+
+export default function FirstPost() {
+    return (
+        <>
+            <HeadMeta title="First Post" description="This is my first post!" />
+            <h1>This is my first post!</h1>
+            <h2 className="title">
+                Back to <Link href="/">Home page</Link>
+            </h2>
+            <Image
+                src="/images/profile.jpg"
+                alt="Jaied Al Sabid"
+                width={420}
+                height={300}
+            />
+        </>
+    );
+}
