f00898f05969871f70344f9d2dd9fd4360c10223
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-11-27 23:33:37 +0600

    fix: Warning

--- pages/components/head-meta/index.js
+++ pages/components/head-meta/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const HeadMeta = ({ title, description }) => {
     return (
         <Head>
-            <title>{title} | Jaied Al Sabid</title>
+            <title>{`${title} | Jaied Al Sabid`}</title>
             <meta name="description" content={description} />
             <link rel="icon" href="/favicon.ico" />
         </Head>
