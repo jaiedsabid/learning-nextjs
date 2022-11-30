96dba8e89c026e773f327e545317a3451f41cd2a
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-11-30 23:16:02 +0600

    Replaced with the Prefix image component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,5 @@
-import Image from 'next/image';
-
 // Local imports.
-import { HeadMeta, Layout } from '../../components';
-import { prefix } from '../../utils/constants';
+import { HeadMeta, Layout, Image } from '../../components';
 
 export default function FirstPost() {
     return (
@@ -10,7 +7,7 @@ export default function FirstPost() {
             <HeadMeta title="First Post" description="This is my first post!" />
             <h1>This is my first post!</h1>
             <Image
-                src={`${prefix}/images/profile.jpg`}
+                src="/images/profile.jpg"
                 alt="Jaied Al Sabid"
                 width={420}
                 height={300}
