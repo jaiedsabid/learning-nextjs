c8d4f90f9184623a45df81966cf5edfb050267bd
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-11-30 22:53:54 +0600

    Updated layout component
    Added back to the home button at the bottom and added profile image and link to home page at the top.

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,7 +1,56 @@
-import styles from './layout.module.scss';
+import Link from 'next/link';
 
-const Layout = ({ children }) => {
-    return <div className={styles.container}>{children}</div>;
+// Local Imports.
+import { Image } from '../index';
+import styles from './layout.module.scss';
+import utilStyles from '../../styles/utils.module.scss';
+
+const name = 'Jaied Al Sabid';
+
+const Layout = ({ children, home }) => {
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <Image
+                            priority
+                            src="/images/profile.jpg"
+                            className={utilStyles.borderCircle}
+                            height={144}
+                            width={144}
+                            alt=""
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <Image
+                                priority
+                                src="/images/profile.jpg"
+                                className={utilStyles.borderCircle}
+                                height={108}
+                                width={108}
+                                alt=""
+                            />
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/" className={utilStyles.colorInherit}>
+                                {name}
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">← Back to home</Link>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Layout;
