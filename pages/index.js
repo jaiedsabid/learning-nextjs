eea15f867a86912e1fdf7e6aa917ef3145b6473d
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-11-30 21:32:08 +0600

    Added comment to the home page file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
+
+// Local imports.
 import styles from '../styles/Home.module.css';
 import { prefix } from '../utils/constants';
 import HeadMeta from '../components/head-meta';
