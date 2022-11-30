5c40d03de29686de12bd212d76072e240fd69588
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-11-30 21:39:05 +0600

    Added stylings to the layout component

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,5 +1,7 @@
+import styles from './layout.module.scss';
+
 const Layout = ({ children }) => {
-    return <div>{children}</div>;
+    return <div className={styles.container}>{children}</div>;
 };
 
 export default Layout;
