8a1db047c59b83116f57df181c65448bc8ac83b4
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-11-30 22:52:16 +0600

    Updated HeadMeta component

--- components/head-meta/index.js
+++ components/head-meta/index.js
@@ -1,11 +1,20 @@
 import Head from 'next/head';
 
+const siteTitle = 'Jaied Al Sabid';
+
 const HeadMeta = ({ title, description }) => {
     return (
         <Head>
-            <title>{`${title} | Jaied Al Sabid`}</title>
+            <title>{`${title} | ${siteTitle}`}</title>
+            <meta name="og:title" content={siteTitle} />
             <meta name="description" content={description} />
             <link rel="icon" href="/favicon.ico" />
+            <meta
+                property="og:image"
+                content={`https://og-image.vercel.app/${encodeURI(
+                    siteTitle
+                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            />
         </Head>
     );
 };
